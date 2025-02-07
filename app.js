b74a44e07b44d9c68dfeedfc9129a6866239a1f5
adityasaini823 <adityasaini823@gmail.com>
2025-02-08 00:31:53 +0530

    improved api code

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.MONGODB_URI)
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
 app.use(cors());
 app.use(express.static('public'));
 
